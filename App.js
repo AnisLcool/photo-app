60392e34d02b33dbe2baac9bca250e81e83d9e6b
anis rbh <mohamedrbh519@gmail.com>
2022-05-05 15:04:08 +0100

    2 - ajout du composant Cam(et Camera)

--- App.js
+++ App.js
@@ -3,44 +3,56 @@ import Home from './components/Home';
 import { useEffect, useState, } from 'react';
 import axios from 'axios';
 
-export default function App() {
-  const [users, setUsers] = useState([]);
-  // useEffect accept 2 argument
-  // une fonction anonyme
-  // un tableau de dépendances
-  console.log('return App.js');
-  useEffect(() => {
-    console.log('useEffect App.js')
-    // synchrone , asynchrone, promise, ajax, fetch, axios => 
-    // faire des appels à une API
-    axios.get('https://api.github.com/users').then((response) => {
+// export default function App() {
+//   const [users, setUsers] = useState([]);
+//   // useEffect accept 2 argument
+//   // une fonction anonyme
+//   // un tableau de dépendances
+//   console.log('return App.js');
+//   useEffect(() => {
+//     console.log('useEffect App.js')
+//     // synchrone , asynchrone, promise, ajax, fetch, axios => 
+//     // faire des appels à une API
+//     axios.get('https://api.github.com/users').then((response) => {
 
-      const data = response.data; // [{...} , {...}]
-      setUsers(data);
+//       const data = response.data; // [{...} , {...}]
+//       setUsers(data);
 
-    }).catch(error => {
+//     }).catch(error => {
 
-      console.log('error : ', error);
-    });
+//       console.log('error : ', error);
+//     });
 
-  }, []);
-  const usersElements = users.map(user => {
-    return (
-      <View>
-        <Text>Id : {user.id}</Text>
-        <Text>Username : {user.login}</Text>
-        <Image style={styles.image} source={{uri: user.avatar_url}}  />
-      </View>
-    )
-  })
+//   }, []);
+//   const usersElements = users.map(user => {
+//     return (
+//       <View>
+//         <Text>Id : {user.id}</Text>
+//         <Text>Username : {user.login}</Text>
+//         <Image style={styles.image} source={{uri: user.avatar_url}}  />
+//       </View>
+//     )
+//   })
+//   return (
+//     <View style={styles.container}>
+//       {/* <Home /> */}
+//     {usersElements}
+
+//     </View>
+//   );
+// }
+
+
+
+const App = () => {
   return (
     <View style={styles.container}>
-      {/* <Home /> */}
-    {usersElements}
-
+      <Home />
     </View>
-  );
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
@@ -49,9 +61,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  image:{
-    width:50,
-    height:50
+  image: {
+    width: 50,
+    height: 50
   }
 });
 
--- components/Cam.jsx
+++ components/Cam.jsx
@@ -1,21 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
-import {useEffect} from 'react';
-
+import { useEffect } from 'react';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { Camera } from 'expo-camera';
 const Cam = () => {
     console.log('return Cam.js')
     useEffect(() => {
         // synchrone , asynchrone, promise, ajax, fetch, axios => 
         // faire des appels à une API
         console.log('useEffect Cam.js')
-      })
+    })
     return (
-        <View>
-            <Text>Cam</Text>
+        <View style={styles.container}>
+            <Camera>
+                <TouchableOpacity onPress={() => { }}>
+                    <Ionicons name='camera-reverse-sharp' size={64} color='green' />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => { }}>
+                    <MaterialIcons name='camera' size={64} color='red' />
+                </TouchableOpacity>
+            </Camera>
         </View>
     )
 }
 
 export default Cam
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 3,
+        flex: 1
+    }
+})
